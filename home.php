c4ff165f65aa40f0b9dcec0310d1894fe425cd2f
Mijael Blanco <blancomijael@gmail.com>
2023-02-09 20:49:58 -0600

    hardcoded

--- home.php
+++ home.php
@@ -97,51 +97,16 @@ confirm_logged_in();
               <option value="2023">2023</option>
             </select>
             <select onchange="getVideo()" id="mes" name="mes">
-              <option value="01">Enero</option>
-              <option value="02">Febrero</option>
-              <option value="03">Marzo</option>
-              <option value="04">Abril</option>
-              <option value="05">Mayo</option>
-              <option value="06">Junio</option>
-              <option value="07">Julio</option>
-              <option value="08">Agosto</option>
-              <option value="09">Septiembre</option>
-              <option value="10">Octubre</option>
-              <option value="11">Noviembre</option>
-              <option value="12">Diciembre</option>
+              <option selected value="02">Febrero</option>
             </select>
             <select onchange="getVideo()" id="dia" name="dia">
               <option value="01">01</option>
               <option value="02">02</option>
               <option value="03">03</option>
-              <option value="04">04</option>
-              <option value="05">05</option>
               <option value="06">06</option>
               <option value="07">07</option>
               <option value="08">08</option>
-              <option value="09">09</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
-              <option value="21">21</option>
-              <option value="22">22</option>
-              <option value="23">23</option>
-              <option value="24">24</option>
-              <option value="25">25</option>
-              <option value="26">26</option>
-              <option value="27">27</option>
-              <option value="28">28</option>
-              <option value="29">29</option>
-              <option value="30">30</option>
-              <option value="31">31</option>
+              <option selected value="09">09</option>
             </select>
             <select onchange="getVideo()" id="hora" name="hora">
               <option value="05">05:00AM</option>
@@ -251,10 +216,10 @@ function getVideo() {
   var multimedios = document.getElementById('multimedios');
 
   // set videos
-  televisa.setAttribute('src', 'media/televisa/' + año + '/' + mes + '/' + dia + '/' + hora + '.MP4');
+  televisa.setAttribute('src', 'media/televisa/' + año + '/' + mes + '/' + dia + '/' + hora + '.mp4');
   televisa.setAttribute('type', 'video/mp4');
 
-  azteca.setAttribute('src', 'media/tva/' + año + '/' + mes + '/' + dia + '/' + hora + '.MP4');
+  azteca.setAttribute('src', 'media/tva/' + año + '/' + mes + '/' + dia + '/' + hora + '.mp4');
   azteca.setAttribute('type', 'video/mp4');
 
   multimedios.setAttribute('src', 'media/mm/' + año + '/' + mes + '/' + dia + '/' + hora + '.mp4');
