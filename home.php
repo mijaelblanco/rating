30b61349303142a11e1b48a7f13504670fa5e6e2
Mijael Blanco <blancomijael@gmail.com>
2023-02-14 06:43:09 -0600

    date

--- home.php
+++ home.php
@@ -106,7 +106,8 @@ confirm_logged_in();
               <option value="07">07</option>
               <option value="08">08</option>
               <option value="09">09</option>
-              <option selected value="10">10</option>
+              <option value="10">10</option>
+              <option selected value="10">13</option>
             </select>
             <select onchange="getVideo()" id="hora" name="hora">
               <option selected value="horario">HORARIO</option>
